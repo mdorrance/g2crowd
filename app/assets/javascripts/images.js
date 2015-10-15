d064a900b619b2045451ad722ffd40fc2f3496eb
Michael Dorrance <dorrancemd@gmail.com>
2015-10-15 15:01:54 -0600

    implemented sorting of the images by height then total height

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -9,12 +9,38 @@ function fetchImages() {
     url: "http://numberonelarge.com/tapjoy/jsonp.php"
   });
 }
+function sumTo(sum) {
+  for (var i = 0; i < array.length; i++) {
+    for (var j = array.length; j > i; j--) {
+      if (array[i] + array[j] === sum) {
+        var x = array[i];
+        var y = array[j];
 
+        array.splice(array[i-1], 1);
+        array.pop();
+        return [x,y];
+      }
+    }
+  }
+}
 function reorderByHeight(images) {
-  var sortedByHeight = images.sort(function (img1, img2) {
+  var pairedImages = [];
+  var sorted = images.sort(function (img1, img2) {
     return img2.height - img1.height;
-  }); //now I need to order by 400px heights and make a new array
+  });
 
+  for (var i = 0; i < sorted.length; i++) {
+    for (var j = sorted.length-1; j > i; j--) {
+      if (sorted[i].height + sorted[j].height === 400) {
+        var x = sorted[i];
+        var y = sorted[j];
+        sorted.splice(j--, 1);
+        sorted.splice(i, 1);
+        pairedImages.push(x,y);
+      }
+    }
+  }
+  return $.merge(pairedImages, sorted);
 };
 
 function renderImagesAndAddEventListeners(images) {
@@ -25,7 +51,7 @@ function renderImagesAndAddEventListeners(images) {
   $images.hide()
          .on('load', function () {
            if (++imagesLoaded === totalNumberOfImages) {
-             var reordered = reorderByHeight($images);
+             var reordered = $( reorderByHeight($images) );
              $('.grid img').remove();
              reordered.appendTo('.grid').show();
            }
